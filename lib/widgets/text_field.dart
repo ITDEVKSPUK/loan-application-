e1ee1eb2ab76cbb74dd577c6e401030bf6ce54b0
Machaaavynn <160084417+Machaaavynn@users.noreply.github.com>
2025-03-13 10:11:09 +0700

    feat: [BRI] custtom textfield and card

--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final String labelText;
   final TextEditingController controller;
   final bool obscureText;
@@ -11,6 +11,8 @@ class CustomTextField extends StatelessWidget {
   final TextStyle labelStyle;
   final bool filled;
   final TextStyle hintStyle;
+  final Color borderColor; // Warna border baru
+  final double borderWidth; // Ketebalan border baru
 
   CustomTextField({
     required this.labelText,
@@ -20,27 +22,98 @@ class CustomTextField extends StatelessWidget {
     required this.heigth,
     required this.width,
     required this.color,
-    required this.labelStyle, required this.filled, required this.hintStyle,
+    required this.labelStyle,
+    required this.filled,
+    required this.hintStyle,
+    this.borderColor = Colors.blue, // Default warna border
+    this.borderWidth = 2.0, // Default ketebalan border
   });
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _isObscured = true;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: heigth,
-      width: width,
+      height: widget.heigth,
+      width: widget.width,
       child: TextField(
-        controller: controller,
-        obscureText: obscureText,
-        keyboardType: keyboardType,
+        controller: widget.controller,
+        obscureText: widget.obscureText ? _isObscured : false,
+        keyboardType: widget.keyboardType,
         decoration: InputDecoration(
-          labelText: labelText,
-          labelStyle: TextStyle(color: color),
-          filled: filled,
-          fillColor: Colors.white, 
-          border: OutlineInputBorder(),
-          hintStyle: TextStyle(
-            color: color, 
-          )
+          labelText: widget.labelText,
+          hintText: widget.labelText,
+          floatingLabelBehavior: FloatingLabelBehavior.never,
+          labelStyle: widget.labelStyle,
+          filled: widget.filled,
+          fillColor: Colors.white,
+
+          // **Custom Outline Border**
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              color: widget.borderColor,
+              width: widget.borderWidth,
+            ),
+          ),
+
+          // **Border saat tidak aktif (default)**
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              color: widget.borderColor.withOpacity(0.5), // Lebih transparan
+              width: widget.borderWidth,
+            ),
+          ),
+
+          // **Border saat fokus**
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              color: widget.borderColor, // Warna lebih jelas saat fokus
+              width: widget.borderWidth + 1, // Tambah ketebalan
+            ),
+          ),
+
+          // **Border saat error**
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              color: Colors.red, // Warna merah saat error
+              width: widget.borderWidth,
+            ),
+          ),
+
+          // **Border saat fokus dalam kondisi error**
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(
+              color: Colors.redAccent,
+              width: widget.borderWidth + 1,
+            ),
+          ),
+
+          hintStyle: widget.hintStyle,
+
+          // **Icon show/hide password**
+          suffixIcon: widget.obscureText
+              ? IconButton(
+                  icon: Icon(
+                    _isObscured ? Icons.visibility_off : Icons.visibility,
+                    color: Colors.grey,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isObscured = !_isObscured;
+                    });
+                  },
+                )
+              : null,
         ),
       ),
     );
