26905ca544ccc19ba0b4894b8bce57438b5b8cab
Machaaavynn <160084417+Machaaavynn@users.noreply.github.com>
2025-04-14 09:39:03 +0700

    fix [BRI] clean up location fetching methods and improve variable handling

--- lib/views/employee/History/controller_location.dart
+++ lib/views/employee/History/controller_location.dart
@@ -1,6 +1,5 @@
 import 'package:get/get.dart';
 import 'package:loan_apllication/API/service/get_location.dart';
-import 'package:loan_apllication/API/models/locations/models_location_prov.dart';
 
 class LocationController extends GetxController {
   var provinces = [].obs;
@@ -22,25 +21,49 @@ class LocationController extends GetxController {
   void fetchProvinces() async {
     try {
       final data = await ApiService.fetchProvinces();
-      print('Fetched provinces: $data');
       provinces.value = data;
+
+      print(">>> Province List IDs: ${data.map((e) => e['pro_idn']).toList()}");
+      print(">>> Current selectedProvinceId: ${selectedProvinceId.value}");
     } catch (e) {
       print('Error fetching provinces: $e');
     }
   }
 
   void fetchRegencies(String provinceId) async {
+    // Simpan ID provinsi yang dipilih
+    selectedProvinceId.value = provinceId;
+
+    // Reset pilihan yang ada sebelumnya
+    selectedRegencyId.value = '';
+    selectedDistrictId.value = '';
+    selectedVillageId.value = '';
+
+    // Fetch kabupaten berdasarkan provinsi baru
     regencies.value = await ApiService.fetchRegencies(provinceId);
+
+    // Kosongkan kecamatan dan desa
     districts.clear();
     villages.clear();
   }
 
   void fetchDistricts(String regencyId) async {
+    selectedRegencyId.value = regencyId;
+
+    // Reset ID kecamatan dan desa
+    selectedDistrictId.value = '';
+    selectedVillageId.value = '';
+
     districts.value = await ApiService.fetchDistricts(regencyId);
     villages.clear();
   }
 
   void fetchVillages(String districtId) async {
+    selectedDistrictId.value = districtId;
+
+    // Reset desa sebelumnya
+    selectedVillageId.value = '';
+
     villages.value = await ApiService.fetchVillages(districtId);
   }
 }
