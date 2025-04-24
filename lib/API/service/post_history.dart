4405b86302ac0cd668b2c1f491c43790594605f0
KasiarAffan <kaisaraffan1@gmail.com>
2025-04-24 10:25:07 +0700

    Fix:[KA] fix report API

--- lib/API/service/post_history.dart
+++ lib/API/service/post_history.dart
@@ -23,7 +23,7 @@ class HistoryService {
     };
     try {
       final response = await dio.post(
-        '/sandbox.ics/v1.0/v1/survey/debitur/inquiry',
+        '/sandbox.ics/v1.0/v1/survey/report',
         data: body,
         options: Options(
           headers: headers,
@@ -32,7 +32,7 @@ class HistoryService {
       return response;
     } on DioException catch (e) {
       throw Exception(
-          'Failed to fetch history: ${e.response?.data ?? e.message}');
+          'Failed to fetch history: ${e.response?.data ?? e.message}${e.response?.statusCode}');
     }
   }
 }
