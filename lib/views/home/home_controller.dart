6ddd3f9ed23a022a15b9609ef6de2c4d975216c1
Machaaavynn <160084417+Machaaavynn@users.noreply.github.com>
2025-05-06 08:36:50 +0700

    Fix [BRI] : search in history

--- lib/views/home/home_controller.dart
+++ lib/views/home/home_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
+import 'package:intl/intl.dart';
 import 'package:loan_application/API/models/history_models.dart';
 import 'package:loan_application/API/service/post_history.dart';
 import 'package:loan_application/core/theme/color.dart';
@@ -34,28 +35,37 @@ class HomeController extends GetxController {
     }
   }
 
-  void filterSearch(String query) {
-    filteredList.value = surveyList
-        .where((item) =>
-            item.fullName.toLowerCase().contains(query.toLowerCase()) ||
-            item.application.purpose
-                .toLowerCase()
-                .contains(query.toLowerCase()))
-        .toList();
-  }
+void filterSearch(String query) {
+  filteredList.value = surveyList.where((item) {
+    final queryLower = query.toLowerCase();
+    final fullName = item.fullName.toLowerCase();
+    final purpose = item.application.purpose.toLowerCase();
+    final date = DateFormat('yyyy-MM-dd').format(item.application.trxDate).toLowerCase();
+    final address = item.sectorCity.toLowerCase();
+    return fullName.contains(queryLower) ||
+           purpose.contains(queryLower) ||
+           date.contains(queryLower) ||
+           address.contains(queryLower);
+  }).toList();
+}
 
   void filterByStatus(String status) {
-    filteredList.value =
-        surveyList.where((item) => "UNREAD" == status).toList();
+    if (status == 'All') {
+      filteredList.value = surveyList;
+      return;
+    }
+    filteredList.value = surveyList
+        .where((item) => item.status?.value.toUpperCase() == status.toUpperCase())
+        .toList();
   }
 
   Color getStatusColor(String status) {
     switch (status.toUpperCase()) {
-      case 'ACCEPTED':
+      case 'DITERIMA':
         return AppColors.greenstatus;
-      case 'DECLINED':
+      case 'DITOLAK':
         return AppColors.redstatus;
-      case 'UNREAD':
+      case 'PROSES':
         return AppColors.orangestatus;
       default:
         return Colors.grey;
