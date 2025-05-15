58a1633b4e75190c4048a1faabd5dca451f483dc
zya <sideofze@gmail.com>
2025-05-15 08:47:01 +0700

    feat [ZE] : Refactor Simulation and Profile UI for improved layout and readability

--- lib/views/Simulation_Calculator/simulation_employee.dart
+++ lib/views/Simulation_Calculator/simulation_employee.dart
@@ -22,50 +22,71 @@ class Simulation_Employe extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: AppColors.pureWhite,
-      appBar: AppBar(
-        backgroundColor: AppColors.pureWhite,
-        title: const Center(child: Text('Simulasi Kredit')),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            LoanInputForm(
-                loanAmountController: controller.loanAmountController,
-                loanTermController: controller.loanTermController,
-                interestRateController: controller.interestRateController),
-            const SizedBox(height: 12),
-            LoanTypeDropdown(loanType: controller.loanType),
-            LoanStartDatePicker(startDate: controller.startDate),
-            CustomButton(
-              text: 'Hitung',
-              onPressed: () => controller.calculateLoan(context),
-              color: AppColors.deepBlue,
-              borderRadius: 8,
-              paddingHorizontal: BorderSide.strokeAlignCenter,
-              paddingVertical: BorderSide.strokeAlignCenter,
-              textStyle: const TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          const SizedBox(height: 20),
+          Container(
+            width: double.infinity,
+            height: 57,
+            color: Colors.white,
+            child: const Center(
+              child: Text(
+                'Simulasi Kredit',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontFamily: 'Outfit',
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
-            const SizedBox(height: 20),
-            Obx(() => controller.monthlyPayment.value > 0
-                ? LoanSummaryAndSchedule(
-                    monthlyPayment: controller.monthlyPayment.value,
-                    totalInterest: controller.totalInterest.value,
-                    totalPayment: controller.totalPayment.value,
-                    loanAmountText: controller.loanAmountController.text,
-                    loanTermText: controller.loanTermController.text,
-                    loanType: controller.loanType.value,
-                    interestRateText: controller.interestRateController.text,
-                    startDate: controller.startDate.value,
-                    repaymentSchedule: controller.repaymentSchedule,
-                  )
-                : const SizedBox()),
-          ],
-        ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  LoanInputForm(
+                    loanAmountController: controller.loanAmountController,
+                    loanTermController: controller.loanTermController,
+                    interestRateController: controller.interestRateController,
+                  ),
+                  const SizedBox(height: 12),
+                  LoanTypeDropdown(loanType: controller.loanType),
+                  LoanStartDatePicker(startDate: controller.startDate),
+                  CustomButton(
+                    text: 'Hitung',
+                    onPressed: () => controller.calculateLoan(context),
+                    color: AppColors.deepBlue,
+                    borderRadius: 8,
+                    paddingHorizontal: BorderSide.strokeAlignCenter,
+                    paddingVertical: BorderSide.strokeAlignCenter,
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Obx(() => controller.monthlyPayment.value > 0
+                      ? LoanSummaryAndSchedule(
+                          monthlyPayment: controller.monthlyPayment.value,
+                          totalInterest: controller.totalInterest.value,
+                          totalPayment: controller.totalPayment.value,
+                          loanAmountText: controller.loanAmountController.text,
+                          loanTermText: controller.loanTermController.text,
+                          loanType: controller.loanType.value,
+                          interestRateText:
+                              controller.interestRateController.text,
+                          startDate: controller.startDate.value,
+                          repaymentSchedule: controller.repaymentSchedule,
+                        )
+                      : const SizedBox()),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
