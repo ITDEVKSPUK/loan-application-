7514e98ff72ad3fa993f02c44141e9b3837c9e96
Machaaavynn <160084417+Machaaavynn@users.noreply.github.com>
2025-06-04 11:28:20 +0700

    fix[ZE]: Refactor DataPinjaman to use CustomAppBar and improve form validation

--- lib/views/inputuserdata/data_pinjaman.dart
+++ lib/views/inputuserdata/data_pinjaman.dart
@@ -4,6 +4,7 @@ import 'package:loan_application/core/theme/color.dart';
 import 'package:loan_application/utils/routes/my_app_route.dart';
 import 'package:loan_application/views/inputuserdata/form_agunan_controller.dart';
 import 'package:loan_application/widgets/InputUserData/financial_form_section.dart';
+import 'package:loan_application/widgets/custom_appbar.dart';
 
 class DataPinjaman extends StatefulWidget {
   const DataPinjaman({super.key});
@@ -17,53 +18,61 @@ final controller = Get.put(CreditFormController());
 class _DataPinjamanState extends State<DataPinjaman> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Data Pinjaman"),
-          backgroundColor: Colors.blue,
-        ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 16),
-              const Text("Tujuan Kredit & Jaminan",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-              const SizedBox(height: 16),
-              FinancialFormSection(controller: controller),
-              const SizedBox(height: 16),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (controller.validateForm()) {
-                      controller.createSurvey();
-                      Get.toNamed(MyAppRoutes.formAgunan);
-                    } else {
-                      Get.snackbar("Error", "Please fill all fields correctly");
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.casualbutton1,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+    return WillPopScope(
+      onWillPop: () async {
+        Get.offAllNamed('/dashboard');
+        return false;
+      },
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: CustomAppBar(
+            title: 'Data Pinjaman',
+            onBack: () => Get.back(),
+          ),
+          body: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 16),
+                const Text(
+                  "Tujuan Kredit & Jaminan",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                const SizedBox(height: 16),
+                FinancialFormSection(controller: controller),
+                const SizedBox(height: 16),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (controller.validateForm()) {
+                        controller.createSurvey();
+                        Get.toNamed(MyAppRoutes.formAgunan);
+                      } else {
+                        Get.snackbar("Error", "Please fill all fields correctly");
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.casualbutton1,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
-                  ),
-                  child: Text(
-                    'Save & Next',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: AppColors.pureWhite,
-                      fontFamily: 'Outfit',
+                    child: Text(
+                      'Save & Next',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: AppColors.pureWhite,
+                        fontFamily: 'Outfit',
+                      ),
                     ),
                   ),
                 ),
-              ),
-              // Add your fee   orm fields here
-            ],
+              ],
+            ),
           ),
         ),
       ),
