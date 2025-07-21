f2d284765ff3bc470e780e75a82769b2528f1dfb
Machaaavynn <160084417+Machaaavynn@users.noreply.github.com>
2025-07-21 12:05:16 +0700

    refactor [BRI] : update filter button styles and remove unused showFilterBottomSheet widget

--- lib/widgets/History/filter_button.dart
+++ lib/widgets/History/filter_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:loan_application/core/theme/color.dart';
-import 'package:loan_application/widgets/History/showfilterbuttom.dart';
 
 class FilterButtons extends StatelessWidget {
   final Function(String) onFilterSelected;
@@ -18,43 +17,42 @@ class FilterButtons extends StatelessWidget {
             child: Row(
               children: [
                 const SizedBox(width: 8), // Padding awal
-                _buildButton('ALL'),
+                _buildButton(
+                  'All',
+                  backgroundColor: AppColors.royalBlue,
+                ),
                 const SizedBox(width: 8),
-                _buildButton('APPROVED'),
+                _buildButton(
+                  'APPROVED',
+                  backgroundColor: AppColors.greenstatus,
+                ),
                 const SizedBox(width: 8),
-                _buildButton('PROGRESS'),
+                _buildButton(
+                  'PROGRESS',
+                  backgroundColor: AppColors.orangestatus,
+                ),
                 const SizedBox(width: 8),
-                _buildButton('DITOLAK'),
+                _buildButton(
+                  'DECLINED',
+                  backgroundColor: AppColors.redstatus,
+                ),
                 const SizedBox(width: 8),
               ],
             ),
           ),
         ),
-
-        // Tombol filter icon
-        const SizedBox(width: 8),
-        ElevatedButton(
-          onPressed: () => showFilterBottomSheet(context, onFilterSelected),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: AppColors.pureWhite,
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          child: Icon(Icons.filter_list_alt,
-              size: 25, color: AppColors.blackLight),
-        ),
       ],
     );
   }
-
-  Widget _buildButton(String text) {
+ Widget _buildButton(String text,
+      {Color? backgroundColor, Color? foregroundColor}) {
     return ElevatedButton(
       onPressed: () => onFilterSelected(text),
       style: ElevatedButton.styleFrom(
-        backgroundColor: AppColors.pureWhite,
-        foregroundColor: AppColors.black,
+        backgroundColor:
+            backgroundColor ?? AppColors.pureWhite, // Fallback ke pureWhite
+        foregroundColor:
+            foregroundColor ?? AppColors.pureWhite, // Fallback ke black
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
         ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,8 +9,8 @@ import device_info_plus
 import file_selector_macos
 import flutter_image_compress_macos
 import flutter_secure_storage_macos
-import network_info_plus
 import geolocator_apple
+import network_info_plus
 import path_provider_foundation
 import url_launcher_macos
 
@@ -19,8 +19,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
